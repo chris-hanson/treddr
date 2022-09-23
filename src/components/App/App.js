508928e2a8be2ad311c5f43cea63483505c0d5a1
Chris Hanson <chris.hanson@envato.com>
2022-09-23 18:53:25 +1000

    remove unused

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { GrInstall } from 'react-icons/gr'
 
